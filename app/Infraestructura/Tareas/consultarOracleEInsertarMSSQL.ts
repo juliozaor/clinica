fa57c766c570ea7bbd0cd1f20ffcfe716d34f25a
Julio Jimenez <zaor.julio@gmail.com>
2024-04-08 14:07:59 -0500

    Ajustes oracle

--- app/Infraestructura/Tareas/consultarOracleEInsertarMSSQL.ts
+++ app/Infraestructura/Tareas/consultarOracleEInsertarMSSQL.ts
@@ -15,12 +15,12 @@ const consultarOracleEInsertarMSSQL = async (tipo: number) => {
     await oracledb.initOracleClient();
 
     connection = await oracledb.getConnection({
-      /* connectString: "PRUEBAS",
-      user: "ADMCES",
-      password: "S*STE#AS2021", */
-      connectString: "SALUD.WORLD",
+      connectString: "PRUEBAS",
       user: "ADMCES",
       password: "S*STE#AS2021",
+      /* connectString: "SALUD.WORLD",
+      user: "ADMCES",
+      password: "S*STE#AS2021", */
     });
     servicioLogs.Oracle("Conectar bd", "Conexión establecida");
     console.log("Conexión establecida con Oracle Database");
